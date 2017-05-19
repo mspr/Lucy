b6d310f72c88c6ad39c0ae11cb5b778869119075
mspr <spriet.maxime.eisti@gmail.com>
2017-05-19 23:06:45 +0200

    Create Qt style and update it in already existing files.

--- mainwindow.h
+++ mainwindow.h
@@ -3,20 +3,18 @@
 
 #include <QMainWindow>
 
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
+  public:
+    explicit MainWindow(QWidget* parent = 0);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+  private:
+    Ui::MainWindow* _ui;
 };
 
 #endif // MAINWINDOW_H
