3568a30bea3dda46e3163bdb507d4fa27cceb36b
mspr <spriet.maxime.eisti@gmail.com>
2017-08-30 16:12:15 +0200

    Add tests for project.

--- tests/project/testproject.h
+++ tests/project/testproject.h
@@ -8,7 +8,12 @@ class TestProject : public QObject
   Q_OBJECT
 
   private slots:
+    void createProject();
     void setFileName();
+    void addTree();
+    void addTree_currentTree();
+    void setCurrentTree_from_droid();
+    void setCurrentTree_from_id();
 };
 
 #endif // TESTINTERVAL_H
