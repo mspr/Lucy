e671820d2bc4bc37b11da40736061ac373e17aa8
mspr <spriet.maxime.eisti@gmail.com>
2017-10-09 17:48:52 +0200

    Update output format for testing. Now a xml file is created for each test suite.

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,18 +8,25 @@
 
 int main(int argc, char* argv[])
 {
+  Q_ASSERT(argc == 4);
+
+  argv[3] = (char*)"testPerson.xml";
   TestPerson testPerson;
   QTest::qExec(&testPerson, argc, argv);
 
+  argv[3] = (char*)"testTree.xml";
   TestTree testTree;
   QTest::qExec(&testTree, argc, argv);
 
+  argv[3] = (char*)"testProject.xml";
   TestProject testProject;
   QTest::qExec(&testProject, argc, argv);
 
+  argv[3] = (char*)"testQueryListModel.xml";
   TestQueryListModel testQueryListModel;
   QTest::qExec(&testQueryListModel, argc, argv);
 
+  argv[3] = (char*)"testQueryClauseTableModel.xml";
   TestQueryClauseTableModel testQueryClauseTableModel;
   QTest::qExec(&testQueryClauseTableModel, argc, argv);
 
